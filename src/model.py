54396238133ddd63cf6e665074f7f382bc51b18d
Coeze <71006291+Coeze@users.noreply.github.com>
2025-04-26 20:39:55 +0100

    Update model.py

--- src/model.py
+++ src/model.py
@@ -150,11 +150,11 @@ class CA:
         return wind_factor
 
 
-    def topography_effect(self, slope):
+    def topography_effect(self, slope, aspect):
         """
         Adjust fire spread probability based on terrain slope.
         """
-        slope_factor = np.exp((3.533 * (np.tan(slope))))
+        slope_factor = np.exp((3.533 * (np.tan(slope)) * np.sin(aspect)))
         return slope_factor
 
 
@@ -222,8 +222,7 @@ class CA:
         precipitation_effect = self.precipitation_effect(self.precipitation)
         p_density = self.ndvi[row, col] * 0.5 + 0.5
 
-        wind_slope_factor = self.sigmoid(wind_effects * topography_effects)
-        adjusted_probability = (self.p0 * (1+highest_veg_prob) * (1+p_density) * wind_slope_factor)/(humidity_effects * precipitation_effect)
+        adjusted_probability = (self.p0 * (1+highest_veg_prob) * (1+p_density) * wind_effects * topography_effects * temperature_effects)/(humidity_effects * precipitation_effect)
         print(f" prob: {self.p0}, we: {wind_effects}, a_prob: {adjusted_probability}, tp: {highest_veg_prob}, p_density: {p_density}, humidity: {humidity_effects}, temperature: {temperature_effects}, precipitation: {precipitation_effect}")
         
         # Ensure probability is in [0, 1] range
