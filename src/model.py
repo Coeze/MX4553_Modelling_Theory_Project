b06d7da25f6023f319f7d1a71185a3e7b1124375
Coeze <71006291+Coeze@users.noreply.github.com>
2025-04-26 23:19:41 +0100

    Update model.py

--- src/model.py
+++ src/model.py
@@ -502,7 +502,7 @@ class CA:
         elif folder_path == "alabama":
             burn_bndy_path = 'data/al3039808817220190514/al3039808817220190514_20190513_20190528_burn_bndy.shp'
             dnbr_path = 'data/al3039808817220190514/al3039808817220190514_20190513_20190528_dnbr.tif'
-            self.set_initial_fire([(5, 75)])
+            self.set_initial_fire([(25, 75)])
         else:
             raise ValueError('Please select a valid fire either: alabama or arizona')
 
