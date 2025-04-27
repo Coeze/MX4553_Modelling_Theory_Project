548eec3af5a8bd95b6899e387906fe096d54bef5
Coeze <71006291+Coeze@users.noreply.github.com>
2025-04-27 14:40:53 +0100

    Update model.py

--- src/model.py
+++ src/model.py
@@ -216,8 +216,8 @@ class CA:
         temperature_effects = self.temperature_effect(temperature=self.temperature)
         precipitation_effect = self.precipitation_effect(self.precipitation)
         p_density = self.ndvi[row, col] * 0.5 + 0.5
-
-        adjusted_probability = self.p0 * (1+highest_veg_prob) * (1+p_density) * wind_effects * topography_effects * temperature_effects * humidity_effects * precipitation_effect
+         # * temperature_effects * humidity_effects * precipitation_effect
+        adjusted_probability = self.p0 * (1+highest_veg_prob) * (1+p_density) * wind_effects * topography_effects
         # print(f" prob: {self.p0}, we: {wind_effects}, a_prob: {adjusted_probability}, tp: {highest_veg_prob}, p_density: {p_density}, humidity: {humidity_effects}, temperature: {temperature_effects}, precipitation: {precipitation_effect}")
         
         # Ensure probability is in [0, 1] range
